deb5cf718b198435a278bafbe0986ff2ec1b71b1
Sayed Ahemmad <143632368+Sayeed-5@users.noreply.github.com>
2026-04-09 22:44:18 +0530

    update index file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
+
+// Register Service Worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log("PWA update available");
+  },
+  onOfflineReady() {
+    console.log("PWA is ready to work offline");
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
